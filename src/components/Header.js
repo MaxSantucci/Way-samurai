7b058f5195da6ea3d2bd3368d06524b691982172
Maksym <maksym.marmura@nure.ua>
2022-12-05 20:22:03 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className="header">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0n4ODoAIA--3sUNXFOtz78IfoaPwViociyQ&usqp=CAU" />
+  </header>
+}
+
+export default Header;
